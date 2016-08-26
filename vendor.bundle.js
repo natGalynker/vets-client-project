20c96b8dceaefc2e375cabee287b6c08cc449191
natGalynker <natalgal@gmail.com>
2016-08-25 21:37:48 -0400

    Final commit
    check for deployment

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(43);
+	module.exports = __webpack_require__(44);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 43:
+/***/ 44:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
