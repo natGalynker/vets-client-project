a6425c42ca968b6805b1c59d11e918d343ef4801
natGalynker <natalgal@gmail.com>
2016-10-15 17:27:48 -0400

    Add Pet image
    add the pet image to the image directory
    add new div called div and test to the html file

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const app = {
-  api: 'https://pets-vets-api.herokuapp.com'
+  // api: 'https://pets-vets-api.herokuapp.com'
+  api: 'http://localhost:3000'
 };
 
 module.exports = app;
