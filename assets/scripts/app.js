43059543bfdf5dd837c9ada5050c8307c86ac28c
natGalynker <natalgal@gmail.com>
2016-10-18 02:02:32 -0400

    Update API
    update api to the endpoint

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const app = {
-  // api: 'https://pets-vets-api.herokuapp.com'
-  api: 'http://localhost:3000'
+  api: 'https://pets-vets-api.herokuapp.com'
+  // api: 'http://localhost:3000'
 };
 
 module.exports = app;
