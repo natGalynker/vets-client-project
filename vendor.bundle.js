a305431e27f028cc1c6084bd5aed9d7d9b1aca5f
natGalynker <natalgal@gmail.com>
2016-10-18 02:10:44 -0400

    Add files after grunt build add the files that were ignored previously

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(44);
+	module.exports = __webpack_require__(45);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 44:
+/***/ 45:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
